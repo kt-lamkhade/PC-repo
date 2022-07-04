a2bea990d06cf3df6dc6c303c07f790c881d0ae8
kt-lamkhade <85397893+kt-lamkhade@users.noreply.github.com>
2022-07-04 22:49:58 +0530

    Update ec2_create_functions.py

--- ec2_create_functions.py
+++ ec2_create_functions.py
@@ -4,9 +4,12 @@ import sys
 import time
 #NAME = input("Please provide server name: ")
 #INSTANCE_TYPE = input("Please provide instance type. e.g: t2.micro : ")
-ec2 = boto3.resource("ec2", region_name="AWS_REGION_NAME")
-ec2_client = boto3.client("ec2", region_name="AWS_REGION_NAME")
-s3_client = boto3.client("s3", region_name="AWS_REGION_NAME")
+NAME = sys.argv[1]
+INSTANCE_TYPE = sys.argv[2]
+AWS_REGION_NAME = sys.argv[3]
+ec2 = boto3.resource("ec2", region_name=AWS_REGION_NAME)
+ec2_client = boto3.client("ec2", region_name=AWS_REGION_NAME)
+s3_client = boto3.client("s3", region_name=AWS_REGION_NAME)
 def create_instance():
     
     New_instances = ec2_client.run_instances(
