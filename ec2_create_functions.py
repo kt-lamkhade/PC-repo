e8632ed0d2ae24844a4ddddbf25cee3454780ded
kt-lamkhade <kt.lamkhade@gmail.com>
2022-07-05 12:47:10 +0000

    update aws cli

--- ec2_create_functions.py
+++ ec2_create_functions.py
@@ -2,7 +2,7 @@
 import boto3
 import sys
 import time
-from /devlopment1/PC-repo/config import *
+#from /devlopment1/PC-repo/config import *
 #NAME = input("Please provide server name: ")
 #INSTANCE_TYPE = input("Please provide instance type. e.g: t2.micro : ")
 print("Instance Created: ", sys.argv)
@@ -10,9 +10,9 @@ NAME = sys.argv[1]
 INSTANCE_TYPE = sys.argv[2]
 AWS_REGION_NAME = sys.argv[3]
 print(NAME," ", INSTANCE_TYPE," ", AWS_REGION_NAME)
-ec2 = boto3.resource("ec2", region_name=AWS_REGION_NAME, aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-ec2_client = boto3.client("ec2", region_name=AWS_REGION_NAME, aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-s3_client = boto3.client("s3", region_name=AWS_REGION_NAME, aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+ec2 = boto3.resource("ec2", region_name=AWS_REGION_NAME)
+ec2_client = boto3.client("ec2", region_name=AWS_REGION_NAME)
+s3_client = boto3.client("s3", region_name=AWS_REGION_NAME)
 def create_instance():
     
     New_instances = ec2_client.run_instances(
