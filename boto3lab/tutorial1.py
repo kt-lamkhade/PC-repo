8ee4a79aae76de484460baf2e29c1b877528b428
kt-lamkhade <85397893+kt-lamkhade@users.noreply.github.com>
2022-07-12 18:32:10 +0530

    Update tutorial1.py

--- boto3lab/tutorial1.py
+++ boto3lab/tutorial1.py
@@ -1,6 +1,7 @@
 import boto3
 aws_resource= boto3.resource("s3")
-bucket=aws_resource.Bucket("kirantechnologytest")
+bucket=aws_resource.Bucket("kirantechnologytest1")
 response = bucket.create(
     ACL='public-read',
 )
+print("response")
