505ca79e1125a6b370b9f9c1125075cbea4bdd13
kt-lamkhade <85397893+kt-lamkhade@users.noreply.github.com>
2022-07-12 18:44:03 +0530

    Update tutorial1.py

--- boto3lab/tutorial1.py
+++ boto3lab/tutorial1.py
@@ -3,8 +3,5 @@ aws_resource= boto3.resource("s3")
 bucket=aws_resource.Bucket("kirantechnologytest11")
 response = bucket.create(
     ACL='public-read',
-    CreateBucketConfiguration={
-        'LocationConstraint': 'us-east-1'
-    },
 )
 print(response)
