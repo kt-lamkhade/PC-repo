5ad95aeb40964ad652705522329c6a8bfa2aaba7
kt.lamkhade <kt.lamkhade@gmail.com>
2022-08-27 07:22:11 +0530

    pit policy #8

--- aws_edrs_config.py
+++ aws_edrs_config.py
@@ -75,7 +75,7 @@ def create_replication_template():
     dataPlaneRouting='PUBLIC_IP',
     defaultLargeStagingDiskType='ST1',
     ebsEncryption='DEFAULT',
-    pitPolicy=["True", 1, 7, 1, "DAY"],
+    pitPolicy=config.get("pitPolicy"),
     replicationServerInstanceType='t2.micro',
     replicationServersSecurityGroupsIDs=[
         'sg-03a18ac09d29c7837',
