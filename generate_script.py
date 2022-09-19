f5f9efbcc2b63d57577daccb4a34180f8909b151
kt.lamkhade <kt.lamkhade@gmail.com>
2022-09-19 17:05:30 +0530

    adding generate_script.py #19

--- generate_script.py
+++ generate_script.py
@@ -6,15 +6,15 @@ import sys
 
 map  = {
     'region': os.environ['AWS_REGION'],
-    "stagingAreaSubnetId": "${SUBNET_ID}",
-    "edrClass": "${EDR_CLASS}"
+    "stagingAreaSubnetId": os.environ['SUBNET_ID'],
+    "edrClass": os.environ['EDR_CLASS']
 }
 with open('tmpfile.json', 'w') as outfile:
     json.dump(map, outfile)
 
 
 map2 = {
-    "sg_id": "${SG_ID}"
+    "sg_id": os.environ['SG_ID']
 }
 with open('tmpsgfile.json', 'w') as outfile:
     json.dump(map2, outfile) 
