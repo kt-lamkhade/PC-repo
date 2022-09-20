f58be83b46f5d5687214322674bb486968c2b323
kt.lamkhade <kt.lamkhade@gmail.com>
2022-09-20 17:10:35 +0530

    test module #11

--- aws_edrs_config.py
+++ aws_edrs_config.py
@@ -136,7 +136,7 @@ def test_module():
         pitPolicy = config.get('pitPolicy2')
     """
     ec2_client = session_call.client('ec2')
-    filters = [{'Name':'tag:Name', 'Values':['stajing*']}, 'VpcId'= 'vpc-0ec4fd6ab42babd17']
+    filters = [{'Name':'tag:Name', 'Values':['stajing*']}, 'VpcId' == 'vpc-0ec4fd6ab42babd17']
 
     sn_all = ec2_client.describe_subnets(Filters=filters,)
     for sn in sn_all['Subnets'] :
